d105c99af0727fc0963c7e6f418f3b30aa7ea928
Jessica <jeessicats@gmail.com>
2025-01-09 18:42:38 -0300

    Getting parts of a String

---
+++ src/section3/people/Substring.java
@@ -0,0 +1,26 @@
+package section3.people;
+
+public class Substring {
+    public static void main(String[] args) {
+        String catName = "matcha";
+        String caNameUpper = catName.substring(0,1).toUpperCase() + catName.substring(1);
+        System.out.println(caNameUpper);
+
+        String name = "willow";
+        String firstUpperLetter = name.substring(0, 1).toUpperCase();
+        String rest = name.substring(1);
+        String myName = firstUpperLetter.concat(rest);
+
+        System.out.println(myName);
+
+        String anotherName = "bilbo";
+        String firstUpperLetter2 = anotherName.substring(0, 1).toUpperCase();
+        String rest2 = anotherName.substring(1);
+        int capacity = firstUpperLetter2.length() + rest2.length();
+        String nameCapitalized = new StringBuilder(capacity)
+                .append(firstUpperLetter2)
+                .append(rest2)
+                .toString();
+        System.out.println(nameCapitalized);
+    }
+}
