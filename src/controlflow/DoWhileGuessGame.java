ac44e0b4c86a2d8ccd093f24c0f15c4942207f75
Jessica <jeessicats@gmail.com>
2025-01-31 21:12:58 -0300

    do while

--- src/controlflow/DoWhileGuessGame.java
+++ src/controlflow/DoWhileGuessGame.java
@@ -2,25 +2,28 @@ package controlflow;
 
 import java.util.Random;
 
-public class WhileLoops {
+public class DoWhileGuessGame {
     public static void main(String[] args) {
         int randomNumber = new Random().nextInt(10) + 1;
+        System.out.printf("The random number is %d%n", randomNumber);
         String guessNumberStr = null;
 
-        while (!"q".equals(guessNumberStr)) {
-
+        int attempts = 1;
+        do {
             guessNumberStr = System.console().readLine("Enter a number between 1 and 10: ");
             if (guessNumberStr.matches("-?\\d{1,2}")) {
                 int guessNumber = Integer.parseInt(guessNumberStr);
 
                 if (guessNumber == randomNumber) {
-                    System.out.printf("The number is %d. You got it!%n", randomNumber);
+                    String tryStr = attempts == 1 ? "try" : "tries";
+                    System.out.printf("The number is %d. You got it in %d %s!%n", randomNumber, attempts, tryStr);
                     return;
                 } else {
+                    attempts++;
                     System.out.printf("You didn't get it!%n");
                 }
             }
-        }
+        }  while (!"q".equals(guessNumberStr));
 
     }
 }
