5c7b8a4579361e5c760b702961164bd1d9c0522c
jeessicats <jeessicats@gmail.com>
2025-07-14 18:03:36 -0300

    removing white space

--- src/section3/text/RemovingWhiteSpace.java
+++ src/section3/text/RemovingWhiteSpace.java
@@ -2,10 +2,12 @@ package section3.text;
 
 public class RemovingWhiteSpace {
     public static void main(String[] args) {
-        String firstName = "    Olivia   ";
-        System.out.format("'%s' ", firstName.strip()); //Remove espaço do começo e do final
-        System.out.format("'%s' ", firstName.stripLeading()); //Remove espaço apenas do começo
-        System.out.format("'%s' ", firstName.stripTrailing()); //Remove espaço apenas no final
+        String firstName = "    Jane   ";
+        System.out.format("'%s' ", firstName.strip()); //Remove espaço do começo e do final -> removes both leading and trailing spaces
+        System.out.format("'%s' ", firstName.stripLeading()); //Remove espaço apenas do começo -> removes the leading space
+        System.out.format("'%s' ", firstName.stripTrailing()); //Remove espaço apenas no final -> removes the trailing space
+
+        System.out.format("'%s'", firstName.stripIndent()); //Remove espaços do começo de cada linha, mas não do final -> removes leading spaces from each line but not the trailing spaces
 
         String poem = """
                 Once upon a midnight dreary, while I pondered, weak and weary,
@@ -21,7 +23,15 @@ public class RemovingWhiteSpace {
         System.out.println("");
         System.out.format("'%s' ", poem.stripIndent());
         System.out.format("'%s' ", firstName.trim()); //Faz basicamente o mesmo que split, é um metodo mais antigo
+        // split is new and more efficient than trim, but trim is still used in some cases. trim methot it is not very intelligent, black spaces are diferent in diferent languages, so it is not very reliable.
 
+        String newPoem = removeWhiteSpaces(poem); // This method removes all spaces from the text, not just leading or trailing spaces
+
+        System.out.println(newPoem);
     }
 
+    public static String removeWhiteSpaces(String text) { // This method needs to be static because it is called from a static context (the main method), so we can use it without creating an instance of the class.
+        return text.replace(" ", "");
+    }
 }
+
