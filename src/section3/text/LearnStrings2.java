a3490e74a528efbcbf9f9d2013cbfc6f124cfef4
jeessicats <jeessicats@gmail.com>
2025-07-14 17:14:53 -0300

    upper and lower casing

--- src/section3/text/LearnStrings2.java
+++ src/section3/text/LearnStrings2.java
@@ -2,7 +2,14 @@ package section3.text;
 
 public class LearnStrings2 {
     public static void main(String[] args) {
-        String myText = "Hello World! Here is my firf text.";
-        System.out.println(myText.replace("firf", "nice"));
+        String myText = "Hello World! Here is my first text.";
+        System.out.println(myText.replace("first", "nice"));
+
+        System.out.println(myText.toUpperCase());
+        System.out.println(myText.toLowerCase());
+
+        String myEmptyText = "";
+        System.out.println(myEmptyText.isEmpty()); // true, because the string is empty. Tells if the length of the string is 0.
+        System.out.println(myEmptyText.isBlank()); // true, because the string is empty and contains no whitespace characters
     }
 }
