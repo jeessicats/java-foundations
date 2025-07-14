2aa8cec6d3f0bf90a562cdf873bdb42e1706fa33
jeessicats <jeessicats@gmail.com>
2025-07-14 17:18:37 -0300

    isEmpty vs isBlank

--- src/section3/text/LearnStrings2.java
+++ src/section3/text/LearnStrings2.java
@@ -9,7 +9,15 @@ public class LearnStrings2 {
         System.out.println(myText.toLowerCase());
 
         String myEmptyText = "";
+        String myBlankText = "   "; // contains only whitespace characters
+
         System.out.println(myEmptyText.isEmpty()); // true, because the string is empty. Tells if the length of the string is 0.
+        System.out.println(myBlankText.isEmpty()); // false, because the string contains whitespace characters. Tells if the length of the string is 0.
+
         System.out.println(myEmptyText.isBlank()); // true, because the string is empty and contains no whitespace characters
+        System.out.println(myBlankText.isBlank()); // true, because the string contains only whitespace characters. Tells if the string is empty or contains only whitespace characters.
+
+        //check if a user input is not blank or empty when you save the information in a database
+        // it's preferable to use isBlank() instead of isEmpty() because it checks for both empty and whitespace-only strings.
     }
 }
