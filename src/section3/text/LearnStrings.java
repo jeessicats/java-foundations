fdda5ead03cc193cd2402edeefabcb1f8b6f61e0
jeessicats <jeessicats@gmail.com>
2025-07-14 17:05:31 -0300

    creating strings with java

--- src/section3/text/LearnStrings.java
+++ src/section3/text/LearnStrings.java
@@ -2,6 +2,19 @@ package section3.text;
 
 public class LearnStrings {
     public static void main(String[] args) {
+
+        String theBlueColor = "blue"; // a reference to the word "blue" is stored in the variable color
+        String anotherBlueColor = "blue"; //string literals are stored in a special area of memory called the string pool.
+
+        System.out.println(theBlueColor == anotherBlueColor); // true, because both variables point to the same string literal in the string pool
+        System.out.println(theBlueColor.equals(anotherBlueColor)); // true, because the content of both strings is the same
+
+        String thePurpleColor = new String("purple"); // Strings are classes in Java, that's why we can use new keyword
+        String anotherPurple = new String("purple"); // Strings are classes in Java, that's why we can use new keyword
+
+        System.out.println(thePurpleColor == anotherPurple); // false, because both variables point to different string objects in the heap
+        System.out.println(thePurpleColor.equals(anotherPurple)); // true, because the content of both strings is the same
+
         String fruit = "apple";
         String anotherFruit = "apple";
         String vegetable = new String("broccoli");
