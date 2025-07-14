497ab675d4d1b5fd575ba4a22b7e4864b058c099
jeessicats <jeessicats@gmail.com>
2025-07-14 18:26:30 -0300

    determining if a text contains a string

--- src/section3/text/Contains.java
+++ src/section3/text/Contains.java
@@ -1,4 +1,12 @@
 package section3.text;
 
 public class Contains {
+    public static void main(String[] args) {
+
+        String myPhrase = "The quick brown fox jumps over the lazy dog";
+
+        System.out.println(myPhrase.contains("quick")); // checks if the phrase contains the word "quick"
+
+        System.out.println(myPhrase.contains("Quick")); // checks if the phrase contains the word "Quick" (case-sensitive, so it returns false)
+    }
 }
