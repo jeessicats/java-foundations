e52a3b1068e87d901aa9d01604459d413abc2709
jeessicats <jeessicats@gmail.com>
2025-07-17 18:13:09 -0300

    contentEqual vs equals

--- src/section3/text/CompareStrings.java
+++ src/section3/text/CompareStrings.java
@@ -8,6 +8,7 @@ public class CompareStrings {
         String flavor1Again = "vanilla";
 
 
+        System.out.println("Comparing strings with compareTo method:");
         System.out.println(flavor1.compareTo(flavor2)); // compares the two strings lexicographically, so if flavor1 is lexicographically less than flavor2, it returns a negative number; if they are equal, it returns 0; if flavor1 is greater, it returns a positive number.
         System.out.println(flavor1.compareToIgnoreCase(flavor2)); // compares the two strings lexicographically, ignoring case differences. If flavor1 is lexicographically less than flavor2, it returns a negative number; if they are equal, it returns 0; if flavor1 is greater, it returns a positive number.
 
@@ -15,6 +16,20 @@ public class CompareStrings {
         System.out.println(flavor1.compareToIgnoreCase(flavor1Again)); // compares the two strings lexicographically, ignoring case differences. If flavor1 is lexicographically less than flavor1Again, it returns a negative number; if they are equal, it returns 0; if flavor1 is greater, it returns a positive number.
 
         compareStringsIgnoreCase(flavor1, flavor1Again); // this method compares two strings lexicographically, ignoring case differences and returns the result
+
+
+        System.out.println("Comparing strings with contentEquals method:");
+
+        // The contentEquals its less sensitive than the equals method, it checks if the content of the two strings is equal, but it doesn't check if the case is the same.
+        // String Buffer its not a string, its a mutable sequence of characters, so it can be modified after its creation.
+        // The contentEquals method is used to compare the content of a String with a StringBuffer or StringBuilder object.
+
+        boolean result = flavor1.contentEquals(flavor1Again); // the variable 'result' will hold the result of the comparison and the result it's not a sring, it's a boolean value.
+        // But the println method expects a string, so we need to convert the boolean value to a string before printing it.
+
+        System.out.println(result); // checks if the two strings are equal, considering case sensitivity. It returns true if they are equal, false otherwise.
+
+        System.out.println(flavor1.equals(flavor1Again)); // checks if the two strings are equal, considering case sensitivity. It returns true if they are equal, false otherwise.
 
     }
 
