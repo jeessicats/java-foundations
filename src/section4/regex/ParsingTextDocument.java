95021054c6bf2797be39a9239fd0d083dfc706c0
jeessicats <jeessicats@gmail.com>
2025-07-21 10:25:47 -0300

    regex parsing a real document

--- src/section4/regex/ParsingTextDocument.java
+++ src/section4/regex/ParsingTextDocument.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 
 public class ParsingTextDocument {
     public static void main(String[] args) {
+
+        // Example of parsing a text document using regex to extract student information
         String transcript = """
                 Student Number:	1234598872			Grade:		11
                 Birthdate:		01/02/2000			Gender:	M
@@ -16,14 +18,20 @@ public class ParsingTextDocument {
 
         String regex = """
                 
-                Student\\sNumber:\\s(?<studentNumber>\\d{10}).* #Grab student number
-                Grade:\\s+(?<grade>\\d{1,2}).* #Grab student grade
-                Birthdate:\\s+(?<birthMonth>\\d{2})/(?<birthDay>\\d{2})/(?<birthYear>\\d{4}).* #Grab student birthdate
-                Gender:\\s+(?<gender>\\w+)\\b.* #Grab student grade
-                State\\sID:\\s+(?<stateId>\\d{10})\\b.* #Grab student grade
-                Cumulative\\sGPA\\s\\(Weighted\\)\\s+(?<weightedGPA>[\\d\\.]+)\\b.* #Grab student grade
-                Cumulative\\sGPA\\s\\(Unweighted\\)\\s+(?<unweightedGPA>[\\d\\.]+)\\b.* #Grab student grade
+                Student\\sNumber:\\s(?<studentNumber>\\d{10}).* #Grab student number - .* matches any character 0 or more times, doesn't include new line or carriage return, thats why we use DOTALL
+                Grade:\\s+(?<grade>\\d{1,2}).* #Grab student grade - \\d{1,2} matches 1 or 2 digits, \\s+ matches one or more whitespace characters
+                Birthdate:\\s+(?<birthMonth>\\d{2})/(?<birthDay>\\d{2})/(?<birthYear>\\d{4}).* #Grab student birthdate - \\d{2} matches 2 digits, \\d{4} matches 4 digits, and the / is a literal character
+                Gender:\\s+(?<gender>\\w+)\\b.* #Grab student grade - \\w+ matches one or more word characters (letters, digits, or underscores), and \\b is a word boundary to ensure we match the whole word
+                State\\sID:\\s+(?<stateId>\\d{10})\\b.* #Grab student grade - \\d{10} matches exactly 10 digits, and \\b is a word boundary to ensure we match the whole word
+                Cumulative\\sGPA\\s\\(Weighted\\)\\s+(?<weightedGPA>[\\d\\.]+)\\b.* #Grab student grade - [\\d\\.]+ matches one or more digits or dots, allowing for decimal numbers
+                Cumulative\\sGPA\\s\\(Unweighted\\)\\s+(?<unweightedGPA>[\\d\\.]+)\\b.* #Grab student grade - [\\d\\.]+ matches one or more digits or dots, allowing for decimal numbers
                 """;
+
+        // Pattern.DOTALL allows the dot (.) to match new line characters, and Pattern.COMMENTS allows whitespace and comments in the regex for better readability
+        // Pattern.COMMENTS allows whitespace and comments in the regex for better readability
+
+        // Flags in computer is something that you can turn on or off to change the behavior of the regex engine.
+        // The pipe (|) is used to combine multiple flags, it means that the regex engine will use both flags at the same time.
 
         Pattern pattern = Pattern.compile(regex, Pattern.DOTALL | Pattern.COMMENTS);
         Matcher matcher = pattern.matcher(transcript);
--- src/section4/regex/RegExpression3.java
+++ src/section4/regex/RegExpression3.java
@@ -2,15 +2,24 @@ package section4.regex;
 
 public class RegExpression3 {
     public static void main(String[] args) {
-        System.out.println("kitty".matches(".....")); //Matches any character
+        System.out.println("kitty".matches(".....")); //Matches any character exactly 5 times
 
-        System.out.println("kitty".matches(".*"));
+        System.out.println("kitty".matches(".*")); //Matches any character 0 or more times
 
-        System.out.println("kitty".matches(".+"));
+        System.out.println("kitty".matches(".+")); //Matches any character 1 or more times
 
-        System.out.println("kitty".matches(".?")); //Would not match
+        System.out.println("kitty".matches(".?")); //Matches any character 0 or 1 times
 
-        System.out.println("kitty".matches("^.....")); // ^ Matches the beginning of a String
+        System.out.println("kitty".matches("^.....")); // ^ Matches the beginning of a String and then matches any character exactly 5 times
         System.out.println("kitty".matches("^.....$")); // $ Matches the final of the String
+        System.out.println("kitty".matches("\\b.....$")); // \b Matches a word boundary, so it matches the beginning of a word and then matches any character exactly 5 times at the end of the String
+
+        System.out.println("kitty".matches("\\W\\W\\W\\W\\W")); // \W Matches any non-word character exactly 5 times
+
+        System.out.println("kitty".matches("\\D")); // \D Matches any non-digit character exactly 5 times
+
+        System.out.println("kitty".matches("\\S")); // \S Matches any non-whitespace character exactly 5 times
+
+        //
     }
 }
