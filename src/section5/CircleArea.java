bc6638950fa7d794ccc5c0eb90649d543151bfce
jeessicats <jeessicats@gmail.com>
2025-07-22 10:37:09 -0300

    circle area

--- src/section5/CircleArea.java
+++ src/section5/CircleArea.java
@@ -6,13 +6,21 @@ public class CircleArea {
         System.out.println(circleArea(3));
     }
 
+    // To create a javadoc, you can use the following command in the terminal:
+    // put the mouse above the method, press /** and then press enter.
+    // Alternatively, you can run the command `javadoc -d doc CircleArea.java` in the terminal.
+    // This will generate the documentation in the 'doc' directory.
+    // You can then view the documentation by opening the index.html file in the 'doc' directory.
+
+
     /**
-     * This method calculates the area of a circle
-     * It uses the formula: area = PI * radius^2
+     * Calculates the area of a circle given its radius.
+     * The formula used is area = π * radius^2, where π (Pi) is a mathematical constant approximately equal to 3.14159.
+     * This method takes the radius as a parameter and returns the calculated area as a double.
      * @param radius
      * @return
      */
     public static double circleArea(double radius) {
-        return Math.PI * Math.pow(radius, 2);
+        return Math.PI * Math.pow(radius, 2); // calculates the area of a circle using the formula area = PI * radius^2
     }
 }
