6d21ae089351c99f279496bcf49ac2c2cd9e28b2
jeessicats <jeessicats@gmail.com>
2025-07-24 16:03:32 -0300

    switch statement

--- src/controlflow/SwitchStatement.java
+++ src/controlflow/SwitchStatement.java
@@ -8,10 +8,14 @@ public class SwitchStatement {
 
         System.out.printf("Generated number is: %d%n", randomNumber);
 
+        // With switch statement there is no logic like there is with if-else statements.
         switch (randomNumber) {
             case 1:
                 System.out.println("First number");
                 break;
+                // break is used to exit the switch block, I need to use it to prevent fall-through behavior
+                // fall-through behavior means that if I don't use break, the code will continue to execute the next case even if it doesn't match
+                // fall-through behavior is useful in some cases, but in this case I want to prevent it
             case 2:
                 System.out.println("Second number");
                 break;
@@ -24,13 +28,24 @@ public class SwitchStatement {
 
         String command = null;
 
-        switch (command) {
-            case "go":
-                System.out.println("Going to switch");
-                break;
-            default:
-                System.out.println("Invalid command");
-                break;
+        // switch statement is more readable than if-else statements when there are many conditions to check and also more efficient
+        // but it is not as flexible as if-else statements, because it only works with specific types of values (like int, String, etc.)
+        // there is no easy way to protect against null values in switch statements, so I need to check for null before using it using a if statement
+        if (command != null) {
+            switch (command) {
+                case "go":
+                    System.out.println("Going to switch");
+                    break;
+                default:
+                    System.out.println("Invalid command");
+                    break;
+            }
+        }
+
+        if ("go".equals(command)) {
+            System.out.println("Going to if");
+        } else {
+            System.out.println("Invalid command");
         }
     }
 }
