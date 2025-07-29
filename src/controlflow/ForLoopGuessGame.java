02c5ef38ee07fc54e266f5d398dfbba23c3832b1
jeessicats <jeessicats@gmail.com>
2025-07-29 12:10:50 -0300

    for loop

--- src/controlflow/ForLoopGuessGame.java
+++ src/controlflow/ForLoopGuessGame.java
@@ -11,6 +11,11 @@ public class ForLoopGuessGame {
         System.out.printf("The random number is %d%n", randomNumber);
         String guessNumberStr = null;
 
+        // Using a for loop to limit the number of attempts to guess the number
+        // The loop will run for a maximum of MAX_ALLOWED_TRIES times
+        // The loop variable attempts starts at 1 and increments by 1 each time the loop runs
+        // The loop will exit if the user guesses the number correctly or if the maximum number of attempts is reached
+        // The loop will run as long as attempts is less than or equal to MAX_ALLOWED_TRIES
         for (int attempts = 1; attempts <= MAX_ALLOWED_TRIES; attempts++) {
             guessNumberStr = System.console().readLine("Enter a number between 1 and 10: ");
             if (guessNumberStr.matches("-?\\d{1,2}")) {
@@ -19,7 +24,11 @@ public class ForLoopGuessGame {
                 if (guessNumber == randomNumber) {
                     String tryStr = attempts == 1 ? "try" : "tries";
                     System.out.printf("The number is %d. You got it in %d %s!%n", randomNumber, attempts, tryStr);
-                    break;
+                    break; // we use break to exit the loop if the user guesses the number correctly
+                    // return is not used here because we want to keep the loop running until the user guesses the number or reaches the maximum number of attempts
+                    // break is used with all kinds of loops, not just for loops
+
+                    // the continue can be used to skip the current iteration of the loop and move to the next iteration, but in this case, we don't need it
                 } else {
                     if (attempts == MAX_ALLOWED_TRIES) {
                         System.out.printf("You tried %d times. The random number is %d. Ending program now. %n", attempts, randomNumber);
--- src/controlflow/MyForLoop.java
+++ src/controlflow/MyForLoop.java
@@ -3,14 +3,22 @@ package controlflow;
 public class MyForLoop {
     public static void main(String[] args) {
 
-        String[] fruits = {"Apple", "Banana", "Mango", "Orange", "Pineapple"};
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange", "Pineapple", "Grape", "Strawberry", "Blueberry"};
 
         for (int counter = 0; counter < fruits.length; counter++) {
             System.out.printf("Fruit at index %d is %s\n", counter, fruits[counter]);
         }
 
+        // Enhanced for loop (also known as for-each loop) to iterate over the array
+        // This loop is more concise and easier to read, especially for arrays and collections
+        // It automatically iterates through each element in the array without needing an index variable.
+        // the fruit variable will take the value of each element in the fruits array one by one, I can name it whatever I want, it doesn't have to be fruit
         for (String fruit : fruits) {
             System.out.printf("Fruit is %s\n", fruit);
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("Current number is %d\n", i);
         }
     }
 }
